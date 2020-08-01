08bd3cfedb12afb4265cf9f1196fcc7295f5ecb7
richrosenthal <rrosenthal2013@gmail.com>
2020-08-01 10:55:45 -0400

    add kepler data/allow application to read csv file

--- mod.ts
+++ mod.ts
@@ -1,7 +1,19 @@
-async function readFile(){
- const data = await Deno.readTextFile("hello.txt");
+import {join} from "https://deno.land/std/path/mod.ts";
+import {BufReader} from "https://deno.land/std/io/bufio.ts";
+import {parse} from "https://deno.land/std/encoding/csv.ts";
 
- console.log(data);
+async function loadPlanetsData(){
+ const path = join(".","kepler_exoplanets_nasa.csv");
+
+ const file = await Deno.open(path);
+ const bufReader = new BufReader(file);
+ const result = await parse(bufReader, {
+   header: true,
+   comment: "#",
+ });
+ Deno.close(file.rid);
+ console.log(result);
+
 }
 
-await readFile();
+await loadPlanetsData();
